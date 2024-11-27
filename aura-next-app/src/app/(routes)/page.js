d9458ea8f3fafcf15a77d4f1ae3744584ad3d2f8
Sebastian <sebastianlenart@gmail.com>
2024-11-27 17:34:32 +0000

    Low styling hero component
    Another very alpha offer and usecase for Hero section component.

--- aura-next-app/src/app/(routes)/page.js
+++ aura-next-app/src/app/(routes)/page.js
@@ -1,8 +1,53 @@
+import Button from "@components/button/Button";
 import GiftCard from "@components/gift-card/GiftCard";
-
+import Hero from "@components/hero/Hero";
 export default function Home() {
   return (
     <main>
+      <Hero
+        content={"# Home Page"}
+        backgroundType={"image"}
+        backgroundSrc={"/main-bg-img.jpeg"}
+      >
+        <p>
+          Aura Reykjavik is a contemporary tattoo studio situated in the heart
+          of Reykjavik, Iceland. The studio prides itself on providing a
+          welcoming and professional atmosphere. We are highly regarded for our
+          unwavering commitment to quality and excellence, along with a
+          personalized customer experience tailored for everyone seeking unique,
+          custom tattoos in a serene and soothing environment.
+        </p>
+        <p>
+          To book a tattoo appointment, please contact our artists directly,
+          ensuring that your journey from concept to creation is seamless and
+          tailored to your needs.
+        </p>
+        <Button link='/contact' text='Contact Us'>
+          Artists
+        </Button>
+      </Hero>
+      <Hero
+        content={"# Home Page"}
+        backgroundType={"video"}
+        backgroundSrc={"video-mini.mp4"}
+      >
+        <p>
+          Aura Reykjavik is a contemporary tattoo studio situated in the heart
+          of Reykjavik, Iceland. The studio prides itself on providing a
+          welcoming and professional atmosphere. We are highly regarded for our
+          unwavering commitment to quality and excellence, along with a
+          personalized customer experience tailored for everyone seeking unique,
+          custom tattoos in a serene and soothing environment.
+        </p>
+        <p>
+          To book a tattoo appointment, please contact our artists directly,
+          ensuring that your journey from concept to creation is seamless and
+          tailored to your needs.
+        </p>
+        <Button link='/contact' text='Contact Us'>
+          Artists
+        </Button>
+      </Hero>
       <GiftCard />
       <h1>Home Page</h1>
       <p>
