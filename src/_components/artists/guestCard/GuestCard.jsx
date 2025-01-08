644de640dd85d4f2827374fff3c6567919c65650
Sebastian <sebastianlenart@gmail.com>
2025-01-08 18:51:31 +0000

    Merge branch 'main' into booking-form

--- src/_components/artists/guestCard/GuestCard.jsx
+++ src/_components/artists/guestCard/GuestCard.jsx
@@ -1,41 +1,28 @@
-import Image from "next/image";
 import Button from "@components/button/Button";
 import dynamic from "next/dynamic";
 
-export default function GuestCard({ artist, idx }) {
-  const Markdown = dynamic(() => import("react-markdown"));
-  const {
-    name,
-    returningArtist,
-    about,
-    image,
-    date,
-    instagramLink,
-    bookingDetails,
-  } = artist;
-  return (
-    <div className='artist-card'>
-      <Image
-        src={image.url}
-        alt={name}
-        width={320}
-        height={480}
-        className='artist-card__img'
-        sizes='(max-width: 575px) 100vw,(max-width: 992px) 50vw, 33vw'
-        priority={idx < 3}
-      />
-      <div className='artist-card__content'>
-        <p className='artist-card__name'>
-          Tattoo artist: {name}{" "}
-          {returningArtist ? "(returning guest artist)" : null}
-        </p>
-        <Markdown className='artist-card__bio'>{about}</Markdown>
-        <p className='artist-card__date'>Guest spot dates: {date}</p>
-        <p className='artist-card__booking-details'>{bookingDetails}</p>
-        <Button link={instagramLink} target='blank' color='black'>
-          @{name}
-        </Button>
-      </div>
-    </div>
-  );
+export default function GuestCard({artist, idx}) {
+    const Markdown = dynamic(() => import("react-markdown"));
+    const {
+        name,
+        image,
+        date,
+        instagramLink,
+        bookingDetails,
+    } = artist;
+    return (
+        <div className='artist-card'>
+            {/* TODO uncomment bg*/}
+            <div className='artist-card__img'
+                 style={{backgroundImage: `url(${image.url})`}}
+            >
+                <div className='artist-card__box guest__box'>
+                    <h2>{name} <strong>({date})</strong></h2>
+                    <Button link={instagramLink} target='blank'>
+                        @{name}
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
 }
