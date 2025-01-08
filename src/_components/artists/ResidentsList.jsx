76d86022d1d71546d43c119f0e459edfc57e45c7
Inuk Ryjewsky <inuk@Inuks-MacBook-Pro.local>
2025-01-08 17:07:52 +0000

    adding residents cards

--- src/_components/artists/ResidentsList.jsx
+++ src/_components/artists/ResidentsList.jsx
@@ -1,7 +1,7 @@
 import ResidentCardMini from "@components/artists/residentCard/ResidentCardMini";
 export default function ResidentsList({ artists }) {
   return (
-    <section className='section artists-list content-center'>
+    <section className='artists-list'>
       {artists.map((artist, idx) => (
         <ResidentCardMini key={artist.id} artist={artist} idx={idx} />
       ))}
--- src/app/(routes)/page.js
+++ src/app/(routes)/page.js
@@ -1,11 +1,16 @@
 import GiftCard from "@components/gift-card/GiftCard";
 import Hero from "@components/hero/Hero";
+import getResidents from "@services/getResidents";
+import ResidentsList from "@components/artists/ResidentsList";
 
 export default async function Home() {
-  return (
-    <main>
-      <Hero/>
-      <GiftCard />
-    </main>
-  );
+    const residents = await getResidents();
+
+    return (
+        <main>
+            <Hero/>
+            <ResidentsList artists={residents}/>
+            <GiftCard/>
+        </main>
+    );
 }
