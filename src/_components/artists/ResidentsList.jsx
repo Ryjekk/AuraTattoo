935ad916597734c0b490893ba0d6d1c791bcc3e4
Inuk Ryjewsky <inuk@Inuks-MacBook-Pro.local>
2025-01-08 23:15:52 +0000

    Adding changes

--- src/_components/artists/ResidentsList.jsx
+++ src/_components/artists/ResidentsList.jsx
@@ -1,7 +1,7 @@
 import ResidentCardMini from "@components/artists/residentCard/ResidentCardMini";
 export default function ResidentsList({ artists }) {
   return (
-    <section className='artists-list'>
+    <section className='artists-list' style={{paddingTop: '0px'}}>
       {artists.map((artist, idx) => (
         <ResidentCardMini key={artist.id} artist={artist} idx={idx} />
       ))}
--- src/app/(routes)/page.js
+++ src/app/(routes)/page.js
@@ -3,6 +3,7 @@ import Hero from "@components/hero/Hero";
 import getResidents from "@services/getResidents";
 import ResidentsList from "@components/artists/ResidentsList";
 import MiniHero from "@components/miniHero/MiniHero";
+import Vision from "@components/vision/vision";
 
 export default async function Home() {
     const residents = await getResidents();
@@ -10,6 +11,9 @@ export default async function Home() {
     return (
         <main>
             <Hero/>
+            <Vision
+                text='Aura Reykjavik is a contemporary tattoo studio situated in the heart of Reykjavik, Iceland. The studio prides itself on providing a welcoming and professional atmosphere. We are highly regarded for our unwavering commitment to quality and excellence, along with a personalized customer experience tailored for everyone seeking unique, custom tattoos in a serene and soothing environment.'
+                subText='To book a tattoo appointment, please contact our artists directly, ensuring that your journey from concept to creation is seamless and tailored to your needs.'/>
             <MiniHero text='Residents' subText='Book with the best'/>
             <ResidentsList artists={residents}/>
             <GiftCard/>
