154fa495e3c2c9b17367ff4cd08fd53ec9966b94
Inuk Ryjewsky <inuk@Inuks-MacBook-Pro.local>
2025-01-09 21:06:19 +0000

    fixing residents

--- src/_components/artists/guestCard/GuestCard.jsx
+++ src/_components/artists/guestCard/GuestCard.jsx
@@ -1,23 +1,19 @@
 import Button from "@components/button/Button";
-import dynamic from "next/dynamic";
 import Image from "next/image";
 
-export default function GuestCard({ artist, idx }) {
-  const Markdown = dynamic(() => import("react-markdown"));
-  const { name, image, date, instagramLink, bookingDetails } = artist;
+export default function GuestCard({ artist }) {
+  const { name, image, date, instagramLink } = artist;
   return (
     <div className='artist-card'>
-      <div className='artist-card__img'>
+      <div className='artist-card__img-wrapper'>
         <Image
           src={image.url}
-          alt='resident image'
+          alt={`${name} image`}
           fill
-          style={{ objectFit: "cover" }}
+          className="artist-card__img"
         />
         <div className='artist-card__box guest__box'>
-          <h2>
-            {name} <strong>({date})</strong>
-          </h2>
+          <h2>{name} <strong>({date})</strong></h2>
           <Button link={instagramLink} target='blank'>
             @{name}
           </Button>
--- src/_components/artists/residentCard/ResidentCardMini.jsx
+++ src/_components/artists/residentCard/ResidentCardMini.jsx
@@ -2,7 +2,7 @@ import Button from "@components/button/Button";
 import Image from "next/image";
 
 export default function ResidentCardMini({ artist, idx }) {
-  const { name, excerpt, mainImage, slug } = artist;
+  const { name, mainImage, slug } = artist;
   return (
     <div className="artist-card">
       <div className="artist-card__img-wrapper">
