78332fc2eb8ab8ec1b3ca3f63c70fe36f060f58b
Inuk Ryjewsky <inuk@Inuks-MacBook-Pro.local>
2025-01-11 13:00:51 +0000

    adding suspense and build fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "@styles/main.scss";
 import localFont from "next/font/local";
 import { Lato } from "next/font/google";
+import {Suspense} from "react";
 
 const OrpheusPro = localFont({
   src: "./fonts/OrpheusPro-regular.otf",
@@ -22,8 +23,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en' className={`${OrpheusPro.variable} ${lato.variable}`}>
-      <body className='main-wrapper'>{children}</body>
-    </html>
+      // TODO add a loading spinner or so
+      <Suspense fallback={<div>Loading data...</div>}>
+        <html lang='en' className={`${OrpheusPro.variable} ${lato.variable}`}>
+          <body className='main-wrapper'>{children}</body>
+        </html>
+      </Suspense>
   );
 }
