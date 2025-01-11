943b9e2b1503430ad6710a487069aee8b9a3be8a
Inuk Ryjewsky <inuk@Inuks-MacBook-Pro.local>
2025-01-11 21:44:29 +0000

    Adding copy to contact us

--- src/_components/contact/ContactHeader.jsx
+++ src/_components/contact/ContactHeader.jsx
@@ -1,9 +1,12 @@
 export default function ContactHeader() {
   return (
       <header className='column__header column__header--left'>
-          <h2 className='column__subtitle'>Contact us subtitle</h2>
+          <h2 className='column__subtitle'>Hit us up with more questions</h2>
           <p className='column__text'>
-              Contact us text
+              Use this form if you want to purchase a gift card, join out team, visit us, or just want to say hi.
+          </p>
+          <p className='column__text'>
+              Urgent case? Call us at <a  className="column__link" href='tel:+3547807819'>+354 780 7819</a>
           </p>
       </header>
   );
