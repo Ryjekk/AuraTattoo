bb91e661e9d9ccb2f5a8e3a74fc85b06d16a0adc
Inuk Ryjewsky <inuk@Inuks-MacBook-Pro.local>
2025-01-11 18:17:40 +0000

    Adding changes QAv1

--- src/_components/artists/guestCard/GuestCard.jsx
+++ src/_components/artists/guestCard/GuestCard.jsx
@@ -1,28 +1,27 @@
-import Button from "@components/button/Button";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function GuestCard({ artist }) {
   const { name, image, date, instagramLink } = artist;
   return (
-    <div className="artist-card">
+    <div className="guest-card">
       <Link
-        className="artist-card__img-wrapper"
+        className="guest-card__img-wrapper"
         href={instagramLink}
         target="blank"
       >
         <>
-          <div className="artist-card__img-overlay"></div>
+          <div className="guest-card__img-overlay"></div>
           <Image
             src={image.url}
             alt={`${name} image`}
             fill
             className="artist-card__img"
           />
-          <div className="artist-card__box">
+          <div className="guest-card__box">
             <h2>{name}</h2>
-            <span className="artist-card__date">{date}</span>
-            <span className="artist-card__link aritst-card__link--guest">
+            <span className="guest-card__date">{date}</span>
+            <span className="artist-card__link guest-card__link">
               Check Instagram
             </span>
           </div>
