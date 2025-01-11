1faa71e0f2fa321056891717c6958f8dcc975959
Inuk Ryjewsky <inuk@Inuks-MacBook-Pro.local>
2025-01-11 15:29:31 +0000

    moving suspense

--- src/app/(routes)/layout.js
+++ src/app/(routes)/layout.js
@@ -1,5 +1,6 @@
 import Header from "@components/header/Header";
 import Footer from "@components/footer/Footer";
+import {Suspense} from "react";
 
 export const metadata = {
   title: "Aura Tattoo ",
@@ -8,10 +9,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <>
+    <Suspense>
       <Header />
       <main className='content-wrapper'>{children}</main>
       <Footer />
-    </>
+    </Suspense>
   );
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import "@styles/main.scss";
 import localFont from "next/font/local";
 import { Lato } from "next/font/google";
-import {Suspense} from "react";
 
 const OrpheusPro = localFont({
   src: "./fonts/OrpheusPro-regular.otf",
@@ -23,11 +22,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-      // TODO add a loading spinner or so
-      <Suspense fallback={<div>Loading data...</div>}>
-        <html lang='en' className={`${OrpheusPro.variable} ${lato.variable}`}>
-          <body className='main-wrapper'>{children}</body>
-        </html>
-      </Suspense>
+    <html lang='en' className={`${OrpheusPro.variable} ${lato.variable}`}>
+      <body className='main-wrapper'>{children}</body>
+    </html>
   );
 }
