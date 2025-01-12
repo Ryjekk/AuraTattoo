f6d2ec7e892968f653c5916a51129ea6fb48b386
Inuk Ryjewsky <inuk@Inuks-MacBook-Pro.local>
2025-01-12 19:40:15 +0000

    sitemap + robots + GA + redirects

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,50 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/join',
+        destination: '/vacancy',
+        permanent: true,
+      },
+      {
+        source: '/studioPolicy',
+        destination: '/privacy',
+        permanent: true,
+      },
+      {
+        source: '/aftercare',
+        destination: '/faq?q=1#qst-1',
+        permanent: true,
+      },
+      {
+        source: '/covid-19',
+        destination: '/faq',
+        permanent: true,
+      },
+      {
+        source: '/malka',
+        destination: '/residents/malkaink',
+        permanent: true,
+      },
+      {
+        source: '/eerie',
+        destination: '/residents/eerieettt',
+        permanent: true,
+      },
+      {
+        source: '/anna',
+        destination: '/residents/sereneneroink',
+        permanent: true,
+      },
+      {
+        source: '/old-blog/:slug',
+        destination: '/new-blog/:slug',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/app/(routes)/layout.js
+++ src/app/(routes)/layout.js
@@ -1,6 +1,7 @@
 import Header from "@components/header/Header";
 import Footer from "@components/footer/Footer";
 import {Suspense} from "react";
+import Analytics from "../../_lib/TagManager";
 
 export const metadata = {
   title: "Aura Tattoo ",
@@ -10,6 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <Suspense>
+      <Analytics />
       <Header />
       <main className='content-wrapper'>{children}</main>
       <Footer />
