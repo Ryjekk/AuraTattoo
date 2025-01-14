fe92fe1117284a944279131e02ec2277db4f79fe
Sebastian <sebastianlenart@gmail.com>
2025-01-14 00:24:01 +0000

    sitemap config update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,13 @@
 module.exports = {
-  siteUrl: 'https://aurareykjavik.com' || 'https://auratattooreykjavik.com',
+  siteUrl: "https://aurareykjavik.com",
+  alternateRefs: [
+    {
+      href: "https://auratattooreykjavik.com",
+      hreflang: "en",
+    },
+  ],
   generateRobotsTxt: true,
-}
+  robotsTxtOptions: {
+    additionalSitemaps: ["https://auratattooreykjavik.com/sitemap.xml"],
+  },
+};
